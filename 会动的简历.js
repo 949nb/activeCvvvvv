e0a9fb291ed92e88da4dbe32eb71a5a05e1e0f21
hy949 <1661346771@qq.com>
2019-03-13 21:50:50 +0800

    增加右边简历功能

--- 会动的简历.js
+++ 会动的简历.js
@@ -1,4 +1,4 @@
-var code = `/* 亲爱的面试官您好，
+var css1 = `/* 亲爱的面试官您好，
  * 我叫张含雨，
  * 我是一名前端工程狮,
  * 这是一个会动的简历～～
@@ -11,13 +11,14 @@ var code = `/* 亲爱的面试官您好，
  */
  html {
     background: rgb(222,222,222);
-    font-size: 16px;
+    font-size: 14px;
  }
   #coder {
     position: fixed;
     left: 10px;
     top: 10px;
     width: 45%;
+    animation: breath 0.5s infinite alternate-reverse;
  }
  /* 给代码加一点高亮～
  */
@@ -25,27 +26,24 @@ var code = `/* 亲爱的面试官您好，
  .token.selector{color: #5096D7;}
  .token.property{color: #905;}
  .token.function{color: #DD4A68;}
-
  #coder { padding:15px;}
  /* 边框去掉～来点阴影～
  */
  #coder{border: none; box-shadow: -1px 1px 7px 3px rgba(0,0,0,0.29);
-         transform: rotateX(-9deg) rotateY(5deg);
  `;
 
 let domPaper = `
 /* 👉右边来张纸吧～要开始写简历啦～
  */
- #paper {
-    border: 5px solid red;
-    position: fixed;
-}`;
+ #paper {background: white;}
+}
+ `;
 
 function writeCode(prefix,code,fn) {
-    let domCoder = document.querySelector('#coder');
     let n = 0;
     let id = setInterval(() => {
         n += 1;
+        let domCoder = document.querySelector('#coder');
         domCoder.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css, 'css');
         styleTag.innerHTML = prefix + code.substring(0, n);
         domCoder.scrollTop = domCoder.scrollHeight;
@@ -53,20 +51,93 @@ function writeCode(prefix,code,fn) {
             window.clearInterval(id);
             fn && fn.call()
         }
-    }, 0);
+    }, 10);
 }
+function writeMarkdown(markdown, fn){
+    let domPaper = document.querySelector('#paper>.content')
+    let n = 0
+    let id = setInterval(() => {
+      n += 1
+      domPaper.innerHTML = markdown.substring(0, n)
+      domPaper.scrollTop = domPaper.scrollHeight
+      if (n >= markdown.length) {
+        window.clearInterval(id)
+        fn && fn.call()
+      }
+    }, 10)
+  }
 
-writeCode('',code,()=>{
-    createPaper(()=>{
-        writeCode(code,domPaper,()=>{})
+writeCode('', css1, ()=>{ // writeCss call the function
+    createPaper(() => {
+      writeCode(css1, domPaper, ()=> {
+        writeMarkdown(md, ()=>{
+          convertMarkdownToHtml(()=>{
+            writeCode(css1 + domPaper, css3, ()=> {
+              console.log('完成')
+            })
+          })
+        })
+      })
     })
-})
-
+  })
 
 function createPaper(fn) {
     let paper = document.createElement('div');
     paper.id = 'paper';
+    var content = document.createElement('pre')
+    content.className = 'content'
+    paper.appendChild(content)
     document.body.appendChild(paper)
     console.log('创建paper完成')
     fn && fn.call()
 }//创建一个paper
+
+
+function convertMarkdownToHtml(fn){
+    var div = document.createElement('div')  
+    div.className = 'html markdown-body'
+    div.innerHTML = marked(md)
+    let markdownContainer = document.querySelector('#paper > .content')
+    markdownContainer.replaceWith(div)
+    fn && fn.call()
+  }
+  
+
+var md = `
+# 自我介绍
+
+我叫张含雨
+2018届毕业生
+毕业于郑州航空工业管理学院：视觉传达专业(全日制本科)
+
+自学前端半年
+- 在学校四年班长，责任心强～
+- 上班过程中自学前端半年，每天坚持写代码，通过学习做了几个项目，后期都传到了GitHub。
+- 用业余时间对流行的VUE框架进行了自学，多学好思，拥有良好的逻辑思维，注重细节。
+- 具有良好的沟通能力，在不同文化和工作人员的背景下出色的独立完成任务，勇于面对挫折和挑战。
+
+# 技能介绍
+
+1. 利用HTML/CSS/JavaScript等Web技术进行产品的开发。
+2. 配合后台开发人员实现产品功能，独立完成Web前端开发任务 
+3. DIV+CSS布局的HTML代码编写，能够书写符合W3C标准的代码，并有严苛的编码风格和良好的编码习惯。
+
+# 项目介绍
+
+1. 苹果风格轮播
+2. XXX 简历
+3. XXX 画板
+
+# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+`
+
+let css3 = `
+/*
+ * 这就是我的会动的简历
+ * 谢谢观看
+ */
+`
